c8907380814d8193e8851be0d64032a33374df9a
Fong <126806274+Phonghigh@users.noreply.github.com>
2025-05-23 00:32:39 +0700

    Update 2025_05_21_161021_create_student_scores_table.php

--- database/migrations/2025_05_21_161021_create_student_scores_table.php
+++ database/migrations/2025_05_21_161021_create_student_scores_table.php
@@ -1,33 +1,25 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void
+class CreateSessionsTable extends Migration
+{
+    public function up()
     {
-        Schema::create('student_scores', function (Blueprint $table) {
-        $table->id();
-        $table->string('sbd')->index(); // Số báo danh
-        $table->float('toan')->nullable()->index();
-        $table->float('ngu_van')->nullable()->index();
-        $table->float('ngoai_ngu')->nullable()->index();
-        $table->float('vat_li')->nullable()->index();
-        $table->float('hoa_hoc')->nullable()->index();
-        $table->float('sinh_hoc')->nullable()->index();
-        $table->float('lich_su')->nullable()->index();
-        $table->float('dia_li')->nullable()->index();
-        $table->float('gdcd')->nullable()->index();
-        $table->string('ma_ngoai_ngu')->nullable()->index();
-        $table->timestamps();
-// … tương tự cho các cột khác
-
-    });
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('student_scores');
+        Schema::dropIfExists('sessions');
     }
-};
-
+}
